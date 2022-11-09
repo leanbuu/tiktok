9f40521841f6cd38ac670fbb53af0a5f75056980
leanbuu <88716598+leanbuu@users.noreply.github.com>
2022-11-09 20:09:34 +0700

    fix loi gui api lien tuc thanh sau khi dung 500ms

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -9,6 +9,7 @@ import AccountItem from '../Layout/components/AccountsItem'
 import 'tippy.js/dist/tippy.css';
 import { useEffect, useState, useRef} from 'react';
 import { SearchIcon } from '../Layout/components/Icons';
+import useDebounce from '../../hooks/Debounce';
 
 const cx = classNames.bind(styles)
 
@@ -18,16 +19,18 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
     useEffect(() => {
-        if(!searchValue.trim()){
+        if(!debounced.trim()){
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
         .then(res => res.json())
         .then(res => {
             setSearchResult(res.data);
@@ -38,7 +41,7 @@ function Search() {
             setLoading(false);
         })
        
-    }, [searchValue]);
+    }, [debounced]);
 
 
     const handleClear = () => {
